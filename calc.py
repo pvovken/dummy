c15da3b62af5398a8cc75e8aa19ee13ff3d84896
Peter Vovken <122158337+pvovken@users.noreply.github.com>
2023-01-31 15:25:04 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,7 +4,7 @@ def calculator():
         print("1. Add")
         print("2. Subtract")
         print("3. Multiply")
-        print("4. Divide")
+        print("4. Divide!")
         choice = input("Enter choice(1/2/3/4): ")
         
         num1 = float(input("Enter first number: "))
